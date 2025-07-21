ae5434c4b68a6a6da195ec14f14b7c241d19b6a4
Katsuhkay <alex_magalhaes13@hotmail.com>
2025-07-21 17:47:03 -0300

    finalizado!!

---
+++ dist/ano--novo.aa69868b.js
@@ -0,0 +1,22 @@
+AOS.init();
+const dataDoEvento = new Date("Dec 31, 2025 23:59:59");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / 1000);
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+    document.getElementById('contador').innerHTML = `${diasAteOEvento} dias, ${horasAteOEvento} horas, ${minutosAteOEvento} minutos, ${segundosAteOEvento}`;
+}, 1000);
+
+//# sourceMappingURL=ano--novo.aa69868b.js.map
